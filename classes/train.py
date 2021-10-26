966ee9d2064a7ddb029c5e3e8bde10ff37a20a87
lukas <>
2021-10-26 12:26:33 +0200

    Getter und Setter von der Train Klasse implementiert #2

---
+++ classes/train.py
@@ -0,0 +1,17 @@
+class Train:
+  def __init__(train, id_train, capacity, start_station, depature_time, speed):
+    train.id = id_train
+    train.capacity = capacity
+    train.start_station = start_station
+    train.depature_time = depature_time
+    train.speed = speed
+  def getId(train):
+    return train.id_train
+  def setId(train,id_train):
+    train.id_train = id_train
+  def setStartStation(train,start_station):
+    train.start_station = start_station
+  def getStartStation(train):
+    return train.start_station
+      
+  
--- train.py
+++
@@ -1,9 +0,0 @@
-class Train:
-  def __init__(train, id_train, capacity, start_station, depature_time, speed):
-    train.id_train = id_train
-    train.capacity = capacity
-    train.start_station = start_station
-    train.depature_time = depature_time
-    train.speed = speed
-  def getID(train):
-    return train.id_train
