f5027da7436bf34697f2e438420bb28f00b400b1
Berkehan Uenal <berkehan14@outlook.de>
2021-11-02 12:53:43 +0100

    zz

--- Scanner.py
+++ Scanner.py
@@ -1,4 +1,4 @@
-num = input ("#Kommentar"+ 
+num =       ("#Kommentar"+ 
              "#Leere Zeilen werden Ignoriert \n" + 
              "#str: ([a-z] | [A-Z] | [0-9] | _ \n)+" +
              "#dec: Dezimahlzahl mit beliebig vielen Nachkommastellen \n" +
@@ -6,9 +6,16 @@ num = input ("#Kommentar"+
              "#Werte-Trenner: 0x20 (SPACE) \n" +
              "#Zeilen-Trenner: 0x0A (NEWLINE) \n")
 print(num)
-name1 = input("Enter name : ")
-print(name1)
+
   
-# Printing type of input value
-print ("type of number", type(num))
-print ("type of name", type(name1))
+
+
+
+mylines = []                                # Declare an empty list.
+with open ("input.txt", "rt") as myfile:    # Open lorem.txt for reading text.
+    for myline in myfile:                   # For each line in the file,
+        mylines.append(myline.rstrip('\n')) # strip newline and add to list.
+
+
+
+    print(mylines)
