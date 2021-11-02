5facb26b6eded993ab850caf6a54cb73b387b558
Berkehan Uenal <berkehan14@outlook.de>
2021-11-02 14:23:44 +0100

    Scanner funktioniert Kommentare und in Klasse gepackt werden

--- Scanner.py
+++ Scanner.py
@@ -1,12 +1,13 @@
 Tstring = '# Strecken: str(ID) str(Anfang) str(Ende) dec(LÃ¤nge) int(KapazitÃ¤t)'
-Trainstring = '# ZÃ¼ge: str(ID) str(Startbahnhof)/* dec(Geschwindigkeit) int(KapazitÃ¤t)'
-Passengersstring = '# Passagiere: str(ID) str(Startbahnhof) str(Zielbahnhof) int(GruppengrÃ¶ÃŸe) int(Ankunftszeit)'
-
+Linesstring = '# ZÃ¼ge: str(ID) str(Startbahnhof)/* dec(Geschwindigkeit) int(KapazitÃ¤t)'
+Trainstring = '# Passagiere: str(ID) str(Startbahnhof) str(Zielbahnhof) int(GruppengrÃ¶ÃŸe) int(Ankunftszeit)'
 
 
 Trainstations = []  
+Lines = []
 Trains = []
 Passengers = []
+
 
 
 mylines = []                                # Declare an empty list.
@@ -14,13 +15,6 @@ with open ("input.txt", "rt") as myfile:    # Open lorem.txt for reading text.
     for myline in myfile:                   # For each line in the file,
         mylines.append(myline.rstrip('\n')) # strip newline and add to list.
 
-
-
-
-for x in range(len(mylines)):
-    print (mylines[x])
-
-print (mylines[x])
 
 i = 1
 
@@ -36,8 +30,8 @@ if mylines [i] == ("[Lines]"):
     print(i)     
     while(True):
         i+=1 
-        Trains.append(mylines[i])
-        if(Trainstring == mylines[i+1]):
+        Lines.append(mylines[i])
+        if(Linesstring == mylines[i+1]):
             break
 
 
@@ -53,29 +47,13 @@ if mylines [i] == ("[Trains]"):
 
 
 
-
-
-
-
-
-
-
 i+=2
 
-if mylines [i] == ("[Passengers]"): 
-    print(i)     
-    while(True):
-        i+=1 
+if mylines [i] == ("[Passengers]"):      
+    for i in range(len(mylines)):
         Passengers.append(mylines[i])
-        if(Passengersstring == mylines[i+1]):
-            break
+        
 
-
-
-print(i) 
-    
-    
-# Strecken: str(ID) str(Anfang) str(Ende) dec(LÃ¤nge) int(KapazitÃ¤t)
 
     
-print(mylines)
+#print(mylines)
