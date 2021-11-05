0cf95e78fa00d2e8bff1dc4d3c63ef1dfd15baf4
Berkehan Uenal <berkehan14@outlook.de>
2021-11-05 12:52:58 +0100

    Input funktioniert

--- Scanner.py
+++ Scanner.py
@@ -1,59 +1,51 @@
-Tstring = '# Strecken: str(ID) str(Anfang) str(Ende) dec(LÃ¤nge) int(KapazitÃ¤t)'
-Linesstring = '# ZÃ¼ge: str(ID) str(Startbahnhof)/* dec(Geschwindigkeit) int(KapazitÃ¤t)'
-Trainstring = '# Passagiere: str(ID) str(Startbahnhof) str(Zielbahnhof) int(GruppengrÃ¶ÃŸe) int(Ankunftszeit)'
-
-
 Trainstations = []  
 Lines = []
 Trains = []
 Passengers = []
-
-
+i = 0
 
 mylines = []                                # Declare an empty list.
 with open ("input.txt", "rt") as myfile:    # Open lorem.txt for reading text.
     for myline in myfile:                   # For each line in the file,
         mylines.append(myline.rstrip('\n')) # strip newline and add to list.
 
+mylines.append("")
 
-i = 1
+while(True):
 
-if mylines [i] == ("[Stations]"):
-    while(True):
-        i+=1 
-        Trainstations.append(mylines[i])
-        if(Tstring == mylines[i+1]):
-            break
-i+=2
+    if mylines [i] == ("[Stations]"):
+        while(True):
+            i+=1 
+            Trainstations.append(mylines[i])
+            if(('#' in mylines[i+1]) or ("" == mylines[i+1])):
+                break
 
-if mylines [i] == ("[Lines]"): 
-    print(i)     
-    while(True):
-        i+=1 
-        Lines.append(mylines[i])
-        if(Linesstring == mylines[i+1]):
-            break
-
-
-i+=2
-
-if mylines [i] == ("[Trains]"): 
-    print(i)     
-    while(True):
-        i+=1 
-        Trains.append(mylines[i])
-        if(Trainstring == mylines[i+1]):
-            break
+    if mylines [i] == ("[Lines]"): 
+        print(i)     
+        while(True):
+            i+=1 
+            Lines.append(mylines[i])
+            if(('#' in mylines[i+1]) or ("" == mylines[i+1])):
+                break
 
 
+    if mylines [i] == ("[Trains]"): 
+        print(i)     
+        while(True):
+            i+=1 
+            Trains.append(mylines[i])
+            if(('#' in mylines[i+1]) or ("" == mylines[i+1])):
+                break
 
-i+=2
 
-if mylines [i] == ("[Passengers]"):      
-    for i in range(len(mylines)):
-        Passengers.append(mylines[i])
-        
-
-
+    if mylines [i] == ("[Passengers]"):      
+        while(True):
+            i+=1 
+            Passengers.append(mylines[i])
+            if(('#' in mylines[i+1]) or ("" == mylines[i+1])):
+                break    
+        break
     
-#print(mylines)
+    i+=1
+print(i)
+
