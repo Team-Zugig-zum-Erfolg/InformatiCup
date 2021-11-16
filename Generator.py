615f90e3a09885862cc122f13d2453b2c1265087
lukas <>
2021-11-16 10:00:08 +0100

    Created class Generator and three functions of this class #7

---
+++ Generator.py
@@ -0,0 +1,39 @@
+from typing import List
+from Station import Station
+from Line import Line
+from classes import Passenger
+import random
+
+class Generator:
+
+    route = []
+    
+    def random_input_generate(self, size_station, size_lines, size_trains, size_pa):
+
+        
+
+        
+        return True
+            
+    def ranom_station_generate(self,number):
+
+        station = []
+        station_name = "S" + str(i)
+        station_capacity = random.randint(1,8)
+        station = [station_name,station_capacity]
+        return station
+
+    def ranom_line_generate(self,number,stations):
+
+        line = []
+        line_number = "L" + str(number)
+        line_end = []
+        return line
+
+     def ranom_train_generate(self,number,stations):
+
+        return train
+
+    def ranom_passenger_generate(self,number,stations):
+
+        return passenger
