3d41f58c69bad5bc6965688141ee747ec7c38b45
lukas <>
2021-11-17 17:12:42 +0100

    Changed classes Linelist and Stationlist #14 and #12

--- Linelist.py
+++ Linelist.py
@@ -2,58 +2,75 @@ from classes import Line
 
 class Linelist:
 
-    line_objects = []    #the line objects of type class Station
+
     lines = []           #the lines with capacities
     
     
     def initial(self, linelist):
-        
-        self.line_objects = linelist
+      
+        line_number = 0
         for line in linelist:
             self.lines.append([])
+            for i in range(0,line[4]):
+                self.lines[line_number].append([])
+            line_number = line_number + 1
         return True    
    
-    def compare_free(self, train, in_line_time, line_number):
+    def compare_free(self, train, start, end, line_number):
 
-        trains_in_line = 0
         earliest_leave_time = -1
         line_capacities = self.lines[line_number]
-        if len(line_capacities) < self.line_objects[line_number].get_capacity():
-            return [True,-1]
-        for train_in_line in line_capacities:
-                if train_in_line[0] <= in_line_time and (train_in_line[1] >= in_line_time):
-                    if earliest_leave_time > train_in_line[1] or earliest_leave_time == -1:
+        for capacity in line_capacities:
+            not_free = 0
+            for train_in_line in capacity:  
+                if self._train_in_line_is_full(train_in_line,start,end):
+                    not_free = 1
+                    if earliest_leave_time == -1:
                         earliest_leave_time = train_in_line[1]
-                    trains_in_line = trains_in_line + 1
-                if trains_in_line == self.line_objects[line_number].get_capacity():
-                    return [False,earliest_leave_time]
-        return [True,-1]
+                    elif earliest_leave_time > train_in_line[1]:
+                        earliest_leave_time = train_in_line[1]
+            if not_free == 0:
+                return [True,-1]
+            
+        return [False,earliest_leave_time]
+
+    def _train_in_line_is_full(self,train_in_line,start,end):
+        return ((train_in_line[1] >= start and train_in_line[0] <= start) or (train_in_line[0] <= end and train_in_line[1] >= end))
+
     
     def add_new_train_in_line(self, train, start, end, line_number):
 
-        self.lines[line_number].append([start,end,train])
-        return True
+        capacity_number = 0
+        for capacity in self.lines[line_number]:
+            free = 1
+            for train_in_line in capacity:
+                if self._train_in_line_is_full(train_in_line,start,end):
+                    free = 0
+                    break
+            if free == 1:
+                self.lines[line_number][capacity_number].append([start,end,train])
+                return True
+            capacity_number = capacity_number + 1
+        return False
 
     def read_trains_from_line(self, line_number):
-        
         trains = []
-        for train_in_line in self.lines[line_number]:
-            if train_in_line[2] not in trains:
-                trains.append(train_in_line[2])
+        for capacity in self.lines[line_number]:
+            for train_in_line in capacity:
+                if train_in_line[2] not in trains:
+                    trains.append(train_in_line[2])
         return trains
 
-
-line1 = Line("L1",["S1","S2"],2,10)
-line2 = Line("L2",["S1","S2"],2,10)
 
 linelist = Linelist()
 
 
-linelist.initial([line1,line2])
+linelist.initial([["L1","S3","S4",12.2,1]])
 
 linelist.add_new_train_in_line("T1",1,12,0)
-linelist.add_new_train_in_line("T2",1,12,0)
-print(linelist.compare_free("T1",6,0))
+print(linelist.add_new_train_in_line("T2",1,12,0))
+
+print(linelist.compare_free("T1",1,12,0))
 print(linelist.read_trains_from_line(0))
 
 
--- Stationlist.py
+++ Stationlist.py
@@ -2,68 +2,108 @@ from classes import Station
 
 class Stationlist:
 
-    station_objects = []    #the station objects of type class Station
+    
     stations = []           #the stations with capacities
     
     
     def initial(self, stationlist):
         
-        self.station_objects = stationlist
+        station_number = 0
         for station in stationlist:
             self.stations.append([])
+            for i in range(0,station[1]):
+                self.stations[station_number].append([])
+            station_number = station_number + 1
         return True    
    
     def compare_free_place(self, train, in_station_time, station_number):
 
-        trains_in_station = 0
         earliest_leave_time = -1
         station_capacities = self.stations[station_number]
-        if len(station_capacities) < self.station_objects[station_number].getCapacity():
-            return [True,-1]
-        for train_in_station in station_capacities:
-                if train_in_station[0] <= in_station_time and (train_in_station[1] >= in_station_time or train_in_station[3] >= in_station_time):
-                    if earliest_leave_time > train_in_station[3] or earliest_leave_time == -1:
-                        earliest_leave_time = train_in_station[3]
+        for capacity in station_capacities:
+            not_free = 0
+            for train_in_station in capacity:
+                if self._train_in_station_is_full(train_in_station,in_station_time):
+                    if earliest_leave_time > train_in_station[3] and train_in_station[3] != -1:
+                            earliest_leave_time = train_in_station[3]
                     elif earliest_leave_time > train_in_station[1]:
                         earliest_leave_time = train_in_station[1]
-                    trains_in_station = trains_in_station + 1
-                if trains_in_station == self.station_objects[station_number].getCapacity():
-                    return [False,earliest_leave_time]
-        return [True,-1]
+                    elif earliest_leave_time == -1:
+                        if train_in_station[3] != -1:
+                            earliest_leave_time = train_in_station[3]
+                        else:
+                            earliest_leave_time = train_in_station[1]
+                    not_free = 1
+                    break
+            if not_free == 0:
+                return [True,-1]
+            
+        return [False,earliest_leave_time]
+
+    def _train_in_station_is_full(self,train_in_station, in_station_time):
+        return (train_in_station[0] <= in_station_time and (train_in_station[1] >= in_station_time or train_in_station[3] >= in_station_time))
+
     
     def add_new_train_in_station(self, train, in_station_time, station_number):
 
-        self.stations[station_number].append([in_station_time,in_station_time + 1,train,-1])
-        return True
+        capacity_number = 0
+        for capacity in self.stations[station_number]:
+            free = 1
+            for train_in_station in capacity:
+                if self._train_in_station_is_full(train_in_station,in_station_time):
+                    free = 0
+                    break
+            if free == 1:
+                self.stations[station_number][capacity_number].append([in_station_time,in_station_time + 1,train,-1])
+                return True
+            capacity_number = capacity_number + 1
+        return False
 
 
     def add_train_leave_time(self, train, leave_time, station_number):
-        capacity = 0
-        for train_in_station in self.stations[station_number]:
-            if train_in_station[2] == train:
-                self.stations[station_number][capacity][3] = leave_time
-                return True
-            capacity = capacity + 1
+
+        capacity_number = 0
+        for capacity in self.stations[station_number]:
+            t=0
+            for train_in_station in capacity:
+                if train_in_station[2] == train and train_in_station[3] == -1:
+                    self.stations[station_number][capacity_number][t][3] = leave_time
+                    return True
+                t=t+1
+            capacity_number = capacity_number + 1
         return False
 
 
     def read_trains_from_station(self, station_number):
         trains = []
-        for train_in_station in self.stations[station_number]:
-            if train_in_station[2] not in trains:
-                trains.append(train_in_station[2])
+        for capacity in self.stations[station_number]:
+            for train_in_station in capacity:
+                if train_in_station[2] not in trains:
+                    trains.append(train_in_station[2])
         return trains
 
 
-station1 = Station("S1",10)
-station2 = Station("S2",20)
+
 
 stationlist = Stationlist()
 
 
-stationlist.initial([station1,station2])
+stationlist.initial([["S1",1],["S2",2]])
+
+
 
 stationlist.add_new_train_in_station("T1",12,0)
+
+
+stationlist.add_new_train_in_station("T2",12,0)
+
+
+print(stationlist.compare_free_place("T1",12,0))
+
+stationlist.add_train_leave_time("T1",14,0)
+
+print(stationlist.compare_free_place("T1",12,0))
+
 print(stationlist.read_trains_from_station(0))
 
 
