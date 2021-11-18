e1383b5b2ad9265f2f5ba091df8fc583d7b9e318
lukas <>
2021-11-18 20:15:56 +0100

    Updated Groups.py

--- Groups.py
+++ Groups.py
@@ -1,71 +1,79 @@
-from typing import List
-from classes import Station
-from classes import Line
 from classes import Passenger
 
 class Groups:
 
     route = []
-    
+        
     def initial(self,passengers):
 
-        if type(passenger) != list:
+        if type(passengers) != list:
             return False
         for passenger in passengers:
             route_number = 0
             added = 0
-            for route_searched in route:
+            for route_searched in self.route:
                 if route_searched[0].get_start_station() == passenger.get_start_station() and route_searched[0].get_end_station() == passenger.get_end_station():
-                    route[route_number].append(passenger)
+                    self.route[route_number].append(passenger)
                     added = 1
                     break
-                route = route + 1
+                self.route = self.route + 1
             if added == 0:
-                route.append([passenger])
+                self.route.append([passenger])
 
         return True
             
-def partition(array, start, end, compare_func):
+    def partition(array, start, end, compare_func):
 
-    pivot = array[start]
-    low = start + 1
-    high = end
+        pivot = array[start]
+        low = start + 1
+        high = end
 
-    while True:
-        while low <= high and compare_func(array[high], pivot):
-            high = high - 1
+        while True:
+            while low <= high and compare_func(array[high], pivot):
+                high = high - 1
 
-        while low <= high and not compare_func(array[low], pivot):
-            low = low + 1
+            while low <= high and not compare_func(array[low], pivot):
+                low = low + 1
 
-        if low <= high:
-            array[low], array[high] = array[high], array[low]
-        else:
-            break
+            if low <= high:
+                array[low], array[high] = array[high], array[low]
+            else:
+                break
 
-    array[start], array[high] = array[high], array[start]
+        array[start], array[high] = array[high], array[start]
 
-    return high
+        return high
      
-def quick_sort(array, start, end, compare_func):
+    def quick_sort(array, start, end, compare_func):
    
-    if start >= end:
-        return
-    p = partition(array, start, end, compare_func)
-    quick_sort(array, start, p-1, compare_func)
-    quick_sort(array, p+1, end, compare_func)
+        if start >= end:
+            return
+        p = partition(array, start, end, compare_func)
+        quick_sort(array, start, p-1, compare_func)
+        quick_sort(array, p+1, end, compare_func)
 
 
-def get_priority(self):
+    def get_priority(self):
  
-    #passenger_list = self #Liste mit den Groups 
-    quick_sort(route, 0, len(route) - 1, lambda x, y: x.target_round > y.target_round)
+        #passenger_list = self #Liste mit den Groups 
+        quick_sort(self.route, 0, len(route) - 1, lambda x, y: x.target_round > y.target_round)
     
-    for Passenger in route:
-        print(Passenger) 
+        for Passenger in route:
+            print(Passenger) 
 
   
-def passengers_arrive(self):
-    return
-def print_output(self):
-    return
+    def passengers_arrive(self):
+        return
+    def print_output(self):
+        return
+
+
+pa1 = Passenger("P1","S1","S2",12,12)
+pa2 = Passenger("P2","S1","S2",12,12)
+
+
+groupstest = Groups()
+groupstest.initial([pa1,pa2])
+
+
+
