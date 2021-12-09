ff20e1aaaa55764b4f8acbf3457e2f439d7c5dff
Berkehan Uenal <berkehan14@outlook.de>
2021-12-09 19:11:37 +0100

    n
    n

--- simulator_go.py
+++ simulator_go.py
@@ -1,11 +1,13 @@
 import subprocess
 import sys
+import os
 
 result = subprocess.run([sys.executable, "-c", "print('ocean')"])
 
 
-subprocess.Popen(["rm","-r","some.file"])
+#subprocess.Popen(["rm","-r","some.file"])
 
 
+os.spawnl(os.P_DETACH, 'main.py')
 
 print(result)
