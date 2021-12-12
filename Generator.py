2864ffaa65151107f906393d1b49cff66105fa29
Berkehan Uenal <berkehan14@outlook.de>
2021-12-12 18:44:23 +0100

    code cleanup Generator

--- Generator.py
+++ Generator.py
@@ -1,9 +1,9 @@
 from classes import Passenger
 import random
 
+
 class Generator:
 
-    
     def random_input_generate(self, size_station, size_lines, size_trains, size_pa):
 
         stations = []
@@ -11,70 +11,68 @@ class Generator:
         trains = []
         passengers = []
 
-
-        for i in range(1,size_station+1):
+        for i in range(1, size_station+1):
             stations.append(self.random_station_generate(i))
 
-        for i in range(1,size_lines+1):
-            lines.append(self.random_line_generate(i,stations,lines))
+        for i in range(1, size_lines+1):
+            lines.append(self.random_line_generate(i, stations, lines))
 
-        for i in range(1,size_trains+1):
-            trains.append(self.random_train_generate(i,stations,trains))
+        for i in range(1, size_trains+1):
+            trains.append(self.random_train_generate(i, stations, trains))
 
-        for i in range(1,size_pa+1):
-            passengers.append(self.random_passenger_generate(i,stations,passengers))
+        for i in range(1, size_pa+1):
+            passengers.append(self.random_passenger_generate(
+                i, stations, passengers))
 
+        return [stations, lines, trains, passengers]
 
-        
-        return [stations,lines,trains,passengers]
-            
-    def random_station_generate(self,number):
+    def random_station_generate(self, number):
 
         station = []
         station_name = "S" + str(number)
-        station_capacity = random.randint(1,8)
-        station = [station_name,station_capacity]
+        station_capacity = random.randint(1, 8)
+        station = [station_name, station_capacity]
         return station
 
-    def random_line_generate(self,number,stations,lines):
+    def random_line_generate(self, number, stations, lines):
 
         size_stations = len(stations)
-        
+
         line_id = "L" + str(number)
-        
+
         line_end_0 = 0
         line_end_1 = 0
         while True:
-            line_end_0 = random.randint(1,size_stations)
-            line_end_1 = random.randint(1,size_stations)
+            line_end_0 = random.randint(1, size_stations)
+            line_end_1 = random.randint(1, size_stations)
             while line_end_1 == line_end_0:
-                line_end_1 = random.randint(1,size_stations)
+                line_end_1 = random.randint(1, size_stations)
             contain = 0
             for line_current in lines:
-                if line_current[1] == ["S"+str(line_end_0),"S"+str(line_end_1)]:
+                if line_current[1] == ["S"+str(line_end_0), "S"+str(line_end_1)]:
                     contain = 1
             if contain == 1:
                 continue
             else:
                 break
-        
-        line_end = ["S"+str(line_end_0),"S"+str(line_end_1)]
-        line_capacity = random.randint(1,20)
-        line_length = random.randint(1,10)
-        
-        return [line_id,line_end,line_length,line_capacity]
 
-    def random_train_generate(self,number,stations,trains):
+        line_end = ["S"+str(line_end_0), "S"+str(line_end_1)]
+        line_capacity = random.randint(1, 20)
+        line_length = random.randint(1, 10)
+
+        return [line_id, line_end, line_length, line_capacity]
+
+    def random_train_generate(self, number, stations, trains):
 
         size_stations = len(stations)
 
         train_id = "T" + str(number)
         train_start_station = "*"
         while True:
-            every = random.randint(1,10)
+            every = random.randint(1, 10)
             if every <= 4:
                 break
-            train_start_station = "S" + str(random.randint(1,size_stations))
+            train_start_station = "S" + str(random.randint(1, size_stations))
             used = 0
             for train in trains:
                 if train[1] == train_start_station:
@@ -88,26 +86,27 @@ class Generator:
                 break
 
         train_speed = round(random.uniform(0, 2), 1)
-        train_capacity = random.randint(1,20)
+        train_capacity = random.randint(1, 20)
 
-        return [train_id,train_start_station,train_speed,train_capacity]
+        return [train_id, train_start_station, train_speed, train_capacity]
 
-    def random_passenger_generate(self,number,stations,passengers):
+    def random_passenger_generate(self, number, stations, passengers):
 
         size_stations = len(stations)
 
         passenger_id = "P" + str(number)
-      
-        passenger_start_station = random.randint(1,size_stations)
-        passenger_end_station = random.randint(1,size_stations)
+
+        passenger_start_station = random.randint(1, size_stations)
+        passenger_end_station = random.randint(1, size_stations)
         while passenger_start_station == passenger_end_station:
-            passenger_end_station = random.randint(1,size_stations)
+            passenger_end_station = random.randint(1, size_stations)
 
-        passenger_group_size = random.randint(1,10)
-        passenger_target_round = random.randint(1,10)
+        passenger_group_size = random.randint(1, 10)
+        passenger_target_round = random.randint(1, 10)
 
-        return [passenger_id,"S"+str(passenger_start_station),"S"+str(passenger_end_station),passenger_group_size,passenger_target_round]
+        return [passenger_id, "S"+str(passenger_start_station), "S"+str(passenger_end_station), passenger_group_size, passenger_target_round]
+
 
 generator = Generator()
 
-print(generator.random_input_generate(4,4,4,4))
+print(generator.random_input_generate(4, 4, 4, 4))
