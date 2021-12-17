02d5b3f8c3921ddea860b09b0791f1de441202df
Berkehan Uenal <berkehan14@outlook.de>
2021-12-17 11:43:40 +0100

    Update test_simulator.py

--- test_simulator.py
+++ test_simulator.py
@@ -1,4 +1,5 @@
-import subprocess, os
+import subprocess
+import os
 
 files_test = os.listdir('test/')
 files_test.remove('testlexicon.txt')
@@ -10,33 +11,38 @@ files_test.remove('test_100_passengers.txt')
 score = 0
 Errlist = []
 
+
 class test_simulator:
 
     def simulator(self):
         global score
         global Errlist
         for i in range(len(files_test)):
-            print('========' + files_test[i] + '========') 
-            p = subprocess.Popen('python main.py' + ' < test/' + files_test[i], shell = True)
+            print('========' + files_test[i] + '========')
+            p = subprocess.Popen('python main.py' +
+                                 ' < test/' + files_test[i], shell=True)
             out, err = p.communicate()
-            print('====' + files_test[i] + '++++gotest' '====') 
-            p = subprocess.run('Bahn-Simulator.exe -input test/' + files_test[i] + ' -output output.txt -verbose', stdout=subprocess.PIPE, shell = True)
+            print('====' + files_test[i] + '++++gotest' '====')
+            p = subprocess.run('Bahn-Simulator.exe -input test/' +
+                               files_test[i] + ' -output output.txt -verbose', stdout=subprocess.PIPE, shell=True)
             print(p.stdout.decode("utf-8"))
-            
+
             if('Printing score' in (p.stdout.decode("utf-8"))):
-                score +=  1
+                score += 1
             else:
                 Errlist.append(files_test[i])
-                
+
         print('Files tested:')
-        print(files_test) 
-        print('\n' + 'Testscore: ' + str(score) + ' from ' + str(len(files_test)) + '\n')
+        print(files_test)
+        print('\n' + 'Testscore: ' + str(score) +
+              ' from ' + str(len(files_test)) + '\n')
 
         if(score == len(files_test)):
             print("Test was successfully completed")
-        else:    
+        else:
             print('List of failed testcases:')
             print('\n'.join(map(str, Errlist)))
+
 
 ts = test_simulator()
 ts.simulator()
