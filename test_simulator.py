26b219fdff50b495d6f67f957d6e2c4dae917520
Berkehan Uenal <berkehan14@outlook.de>
2021-12-17 11:26:13 +0100

    Update test_simulator.py

--- test_simulator.py
+++ test_simulator.py
@@ -1,12 +1,8 @@
-import subprocess, signal, time, os, io
-import pathlib
-import sys
-import argparse
+import subprocess, os
 
 files_test = os.listdir('test/')
 files_test.remove('testlexicon.txt')
 files_test.remove('.DS_Store')
-
 files_test.remove('test_100_passengers.txt')
 
 #files_test = ['test_1', 'test_2', 'test_3', 'test_4']
@@ -14,47 +10,33 @@ files_test.remove('test_100_passengers.txt')
 score = 0
 Errlist = []
 
-for i in range(len(files_test)):
-   
-    print('========' + files_test[i] + '========') 
-    
-    p = subprocess.Popen('python main.py' + ' < test/' + files_test[i], shell = True)
-    out, err = p.communicate()
+class test_simulator:
 
-    print('====' + files_test[i] + '++++gotest' '====') 
-    
-    p = subprocess.run('Bahn-Simulator.exe -input test/' + files_test[i] + ' -output output.txt -verbose', stdout=subprocess.PIPE, shell = True)
-    print(p.stdout.decode("utf-8"))
-    
-    if('Printing score' in (p.stdout.decode("utf-8"))):
-        score = score + 1
-    else:
-        Errlist.append(files_test[i])
-        
-print('Files tested:')
-print(files_test) 
-print('\n' + 'Testscore: ' + str(score) + ' from ' + str(len(files_test)) + '\n')
+    def simulator(self):
+        global score
+        global Errlist
+        for i in range(len(files_test)):
+            print('========' + files_test[i] + '========') 
+            p = subprocess.Popen('python main.py' + ' < test/' + files_test[i], shell = True)
+            out, err = p.communicate()
+            print('====' + files_test[i] + '++++gotest' '====') 
+            p = subprocess.run('Bahn-Simulator.exe -input test/' + files_test[i] + ' -output output.txt -verbose', stdout=subprocess.PIPE, shell = True)
+            print(p.stdout.decode("utf-8"))
+            
+            if('Printing score' in (p.stdout.decode("utf-8"))):
+                score +=  1
+            else:
+                Errlist.append(files_test[i])
+                
+        print('Files tested:')
+        print(files_test) 
+        print('\n' + 'Testscore: ' + str(score) + ' from ' + str(len(files_test)) + '\n')
 
-if(score == len(files_test)):
-    print("Test was successfully completed")
-else:    
-    print('List of failed testcases:')
-    print('\n'.join(map(str, Errlist)))
+        if(score == len(files_test)):
+            print("Test was successfully completed")
+        else:    
+            print('List of failed testcases:')
+            print('\n'.join(map(str, Errlist)))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ts = test_simulator()
+ts.simulator()
