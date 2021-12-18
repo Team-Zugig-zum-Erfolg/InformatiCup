a393df9031a2797863f360d2864ae7fb2d47472c
lukas <lukas-dreyer@outlook.de>
2021-12-18 11:17:34 +0100

    Added delay time tests to unittests for class Stationslist

--- unittest/unittest_stationlist.py
+++ unittest/unittest_stationlist.py
@@ -23,14 +23,14 @@ for station in stations:
         stationlist.append(station.to_list())
 
 
-test_stationlist = Stationlist(stationlist,trains,None)
+test_stationlist = Stationlist(stationlist,trains)
 
 
 
 test_stationlist.stations[2][0].append(TrainInStation(0,1,trains[0],1,2))
 test_stationlist.stations[2][0].append(TrainInStation(4,4,trains[1],None,2))
 
-
+test_stationlist.stations[1][0].append(TrainInStation(4,5,trains[0],5,2))
 	
 class Testing(unittest.TestCase):
 
@@ -38,6 +38,10 @@ class Testing(unittest.TestCase):
 		self.assertEqual(test_stationlist.compare_free_place(TrainInStation(3,3,trains[2],4,2)),[False,-1])
 		self.assertEqual(test_stationlist.compare_free_place(TrainInStation(3,3,trains[2],3,2)),[True,-1])
 		self.assertEqual(test_stationlist.compare_free_place(TrainInStation(2,3,trains[2],3,2)),[True,-1])
+
 		
+	def test_delay_time(self):
+		self.assertEqual(test_stationlist.compare_free_place(TrainInStation(3,3,trains[1],4,1)),[False,6])
+		self.assertEqual(test_stationlist.compare_free_place(TrainInStation(3,3,trains[1],3,1)),[True,-1])
 				
 unittest.main()
