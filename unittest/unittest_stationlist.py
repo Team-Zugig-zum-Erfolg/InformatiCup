fd5e9effdfab978ebd44702e96958d826adb31e3
Berkehan Uenal <berkehan14@outlook.de>
2021-12-21 12:19:20 +0100

    Update unittest_stationlist.py

--- unittest/unittest_stationlist.py
+++ unittest/unittest_stationlist.py
@@ -1,6 +1,4 @@
-import unittest
-import sys
-import os
+import unittest, sys, os
 
 path = os.path.abspath(os.getcwd())
 sys.path.append(path+'\\..')
@@ -10,9 +8,7 @@ from classes.Train import Train
 from classes.Station import Station
 from classes.TrainInStation import TrainInStation
 
-
 __unittest = True
-
 
 stations = [Station(1,1),Station(2,1)]
 trains = [Train(1,Station(-1,1),1,10),Train(2,Station(-1,1),1,10),Train(3,Station(-1,1),1,10)]
@@ -22,14 +18,10 @@ stationlist = []
 for station in stations:
         stationlist.append(station.to_list())
 
-
 test_stationlist = Stationlist(stationlist,trains)
-
-
 
 test_stationlist.stations[2][0].append(TrainInStation(0,1,trains[0],1,2))
 test_stationlist.stations[2][0].append(TrainInStation(4,4,trains[1],None,2))
-
 test_stationlist.stations[1][0].append(TrainInStation(4,5,trains[0],5,2))
 	
 class Testing(unittest.TestCase):
