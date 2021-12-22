6dcc3311bbfb15d060011de7b2a4ea2e2c9856d7
Berkehan Uenal <berkehan14@outlook.de>
2021-12-22 18:38:40 +0100

    Added new Unittest Linelist

---
+++ abfahrt/unittest/unittest_linelist.py
@@ -0,0 +1,38 @@
+import unittest, sys, os
+
+path = os.path.abspath(os.getcwd())
+sys.path.append(path+'\\..')
+
+from Stationlist import Stationlist
+from classes.Train import Train
+from classes.Station import Station
+from classes.Line import Line
+from classes.TrainInStation import TrainInStation
+from classes.TrainInLine import TrainInLine
+
+__unittest = True
+# self.id, self.start.id, self.end.id, self.length, self.capacity
+line = [Line(Station(1,1), Station(2,1), 10, 2)]
+trains = [Train(1,Station(1,1),1,10), Train(2,Station(3,2),1,10), Train(3,Station(5,3),1,10), Train(4,Station(6,3),1,10)]
+
+stationlist = []
+
+for station in stations:
+        stationlist.append(station.to_list())
+
+test_stationlist = Stationlist(stationlist,trains)
+
+test_stationlist.stations[1][0].append(TrainInStation(0,1,trains[0],1,2))
+
+	
+class Testing(unittest.TestCase):
+
+	def test_between_two_trains(self):
+		self.assertEqual(test_stationlist.compare_free_place(TrainInStation(3,3,trains[2],4,2)),[False,-1])
+
+		
+	def test_delay_time(self):
+		self.assertEqual(test_stationlist.compare_free_place(TrainInStation(3,3,trains[1],4,1)),[False, -1])
+			
+
+unittest.main()
