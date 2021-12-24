97f77fc1b339ded7a16ab0e57d7bcf7fa16abe68
lukas <lukas-dreyer@outlook.de>
2021-12-24 10:35:34 +0100

    Refactoring in test_generator.py

--- abfahrt/test_generator.py
+++ abfahrt/test_generator.py
@@ -9,29 +9,27 @@ class test_generator:
     def simulator(self):
 
         generator = Generator()
-        score = 0
-        tests = 10
+        test_number = 0
+        test_amount = 10
 
-        for i in range(tests):
+        for i in range(test_amount):
+            test_number += 1
             generator.random_input_generate_file(60,1000,10,20,10,100,20,100,60,200) 
             p1 = subprocess.run('python main.py < output_generated.txt', capture_output=True,shell=True)
 
             if 'Too many trains in station at beginning' in p1.stderr.decode("utf-8"):
                 print("Skipped (because input is not valid (too many trains at station at beginning))")
-                score += 1
                 continue
             
             p2 = subprocess.run('Bahn-Simulator.exe -input output_generated.txt -output output.txt -verbose', stdout=subprocess.PIPE, shell=True)
-            #print(p.stdout.decode("utf-8"))
 
             if('Printing score' in (p2.stdout.decode("utf-8"))):
-                score += 1
-                print("Successful: "+str(score))
+                print("Successful test: "+str(test_number))
             else:
-                print("Error")
+                print("Error in test: "+str(test_number))
                 break
 
-        print(str(score)+"/"+str(tests))
+        print("Successful tests: "+str(test_number)+"/"+str(test_amount))
 
 
 ts = test_generator()
