e717fb4c8f651267ea7a9f374f549879a17a8bef
Berkehan Uenal <berkehan14@outlook.de>
2021-12-24 15:31:35 +0100

    add new test test_find_only_one_line_between_stations to unittest_travelcenter

--- abfahrt/unittest/unittest_travelcenter.py
+++ abfahrt/unittest/unittest_travelcenter.py
@@ -16,28 +16,36 @@ __unittest = True
 
 stations = [Station(1,1), Station(2,1), Station(3,2),  Station(4,2), Station(5,3), Station(6,3)]
 trains = [Train(1,Station(1,1),1,10), Train(2,Station(3,2),1,10), Train(3,Station(5,3),1,10), Train(4,Station(6,3),1,10)]
-lines = [Line(1,stations[0],stations[1],1,1)]
+lines = [Line(1,stations[0],stations[1],1,1), Line(2,stations[1],stations[2],1,1), Line(2,stations[2],stations[3],1,1), Line(3,stations[3],stations[4],1,1), Line(4,stations[1],stations[3],1,1) , Line(5,stations[0],stations[2],1,1)]
 
 stationlist = []
 for station in stations:
-        stationlist.append(station.to_list())
+	stationlist.append(station.to_list())
 
 linelist = []
 for line in lines:
-        linelist.append(line.to_list())
+	linelist.append(line.to_list())
 
 test_linelist = Linelist(linelist)
 test_stationlist = Stationlist(stationlist,trains)
 
 test_travelcenter = Travel_Center(stationlist,linelist,trains)
-
-test_stationlist.stations[1][0].append(TrainInStation(0,1,trains[0],1,2))
-test_stationlist.stations[2][0].append(TrainInStation(4,4,trains[1],None,2))
-test_stationlist.stations[3][0].append(TrainInStation(4,5,trains[2],5,2))
 	
 class Testing(unittest.TestCase):
 
 	def test_get_stations_by_line(self):
-		self.assertEqual(Travel_Center.get_stations_by_line(1),[stations[0],stations[1]])
+		self.assertEqual(Travel_Center.get_stations_by_line(1), [stations[0] ,stations[1]], [True, -1])
+		self.assertEqual(Travel_Center.get_stations_by_line(2), [stations[1], stations[2]], [True, -1])
+		self.assertEqual(Travel_Center.get_stations_by_line(3), [stations[2], stations[3]], [True, -1])
+		self.assertEqual(Travel_Center.get_stations_by_line(4), [stations[3], stations[4]], [True, -1])
+		self.assertEqual(Travel_Center.get_stations_by_line(5), [stations[1], stations[3]], [True, -1])		
+	
+	def test_find_only_one_line_between_stations(self):
+                #(self,start_station_id,end_station_id):   # return [length,[line]]
+		self.assertEqual(Travel_Center.find_only_one_line_between_stations(self, 1, 2), [1, [1]])
+		self.assertEqual(Travel_Center.find_only_one_line_between_stations(self, 1, 3), [1, [5]])
+		self.assertEqual(Travel_Center.find_only_one_line_between_stations(self, 2, 3), [1, [2]])
+		self.assertEqual(Travel_Center.find_only_one_line_between_stations(self, 3, 2), [1, [2]])
+		self.assertEqual(Travel_Center.find_only_one_line_between_stations(self, 2, 4), [1, [4]])
 
 unittest.main()
