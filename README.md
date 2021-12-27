3c7c27b7098de6993688b69411e460b71cb5426a
Berkehan Uenal <berkehan14@outlook.de>
2021-12-27 16:13:33 +0100

    added argumentparser to test_generator

--- README.md
+++ README.md
@@ -6,9 +6,14 @@ main:
 
 python -m abfahrt [< input.txt]
 
+Example:
+python -m abfahrt < abfahrt/testfiles/test_1.txt
+
+
 test_simulator:
 
 python -m abfahrt.testutils.test_simulator
+
 
 test_generator:
 
--- abfahrt/testutils/test_generator/test_generator.py
+++ abfahrt/testutils/test_generator/test_generator.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, argparse, sys
 
 from abfahrt.Generator import Generator
 
@@ -8,13 +8,34 @@ class test_generator:
 
         generator = Generator()
         test_number = 0
-        test_amount = 10
+        
+        parser = argparse.ArgumentParser()
+        parser.add_argument('check',type= str, default = "10,60,1000,40,20,10,100,20,100,60,200" ,nargs='?', help="test_amount, size_station, size_lines, size_trains, size_pa, sc_max, lc_max (lc=line capacity), ll_max (ll=line length), tc_max (tc = train capacity), pgs_max (pgs = passenger group size), ptr_max(passenger target round")
+        args = parser.parse_args()
+               
+        if(args.check == "10,60,1000,40,20,10,100,20,100,60,200"):  
+            print("please check documentation for the use of this function and be more creative")
+ 
+        if(args.check != ""):  
+            command = args.check.split(',')
+               
+        if len(command) < 11:
+            print("error, to few arguments")
+            sys.exit()
+        if len(command) > 11:
+            print("error, to many arguments")
+            sys.exit()
+
+
+        test_amount = int(command[0])
 
         for i in range(test_amount):
             test_number += 1
             #random_input_generate_file(size_station, size_lines, size_trains, size_pa, sc_max, lc_max (lc=line capacity), ll_max (ll=line length),
             #                           tc_max (tc = train capacity), pgs_max (pgs = passenger group size), ptr_max(passenger target round))
-            generator.random_input_generate_file(60,1000,40,20,10,100,20,100,60,200) 
+            
+            generator.random_input_generate_file(int(command[1]), int(command[2]), int(command[3]), int(command[4]), int(command[5]), int(command[6]), int(command[7]), int(command[8]), int(command[9]), int(command[10])) 
+
             p1 = subprocess.run('python -m abfahrt < output_generated.txt', capture_output=True,shell=True)
 
             if 'error' in p1.stderr.decode("utf-8"):
