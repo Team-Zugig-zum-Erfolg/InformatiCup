e9c257e7d9acf15df0e4c3e8b5380e9b5ff5eec0
Berkehan Uenal <berkehan14@outlook.de>
2021-12-28 15:38:33 +0100

    add more argparse

--- abfahrt/testutils/test_generator/test_generator.py
+++ abfahrt/testutils/test_generator/test_generator.py
@@ -10,31 +10,87 @@ class test_generator:
         test_number = 0
         
         parser = argparse.ArgumentParser()
-        parser.add_argument('input',type= str, default = "" ,nargs='?', help="Use this Format with Intengers = test amount, size stations, size lines, size_trains, size passengers, station capacity max, line capacity max, line length max, train capacity max, passenger group size max, passenger target round")
+        parser.add_argument('test_amount',
+                            type= int,
+                            default = "",
+                            nargs='?',
+                            help="Use this Format with Intengers = test amount size stations size lines size trains size passengers station capacity max line capacity max line length max train capacity max passenger group size max passenger target round")
+        
+        parser.add_argument('size_stations',
+                            type= int,
+                            default = "",
+                            nargs='?')
+            
+        parser.add_argument('size_lines',
+                            type= int,
+                            default = "",
+                            nargs='?')  
+      
+        parser.add_argument('size_trains',
+                            type= int,
+                            default = "",
+                            nargs='?')                
+
+        parser.add_argument('size_passengers',
+                            type= int,
+                            default = "",
+                            nargs='?') 
+
+        parser.add_argument('station_capacity_max',
+                            type= int,
+                            default = "",
+                            nargs='?') 
+
+        parser.add_argument('line_capacity_max',
+                            type= int,
+                            default = "",
+                            nargs='?') 
+
+        parser.add_argument('line_length_max',
+                            type= int,
+                            default = "",
+                            nargs='?') 
+
+        parser.add_argument('train_capacity_max',
+                            type= int,
+                            default = "",
+                            nargs='?') 
+
+        parser.add_argument('passenger_group_size_max',
+                            type= int,
+                            default = "",
+                            nargs='?')                            
+
+        parser.add_argument('passenger_target_round',
+                            type= int,
+                            default = "",
+                            nargs='?') 
+
         args = parser.parse_args()
+    
+     #  if(args.input == ""):
+     #       print("use --help for more informations or check documentation")
+     #       sys.exit()
 
-        if(args.input == ""):
-            print("use --help for more informations or check documentation")
-            sys.exit()
-
-        if(args.input != ""):  
-            command = args.input.split(',')
+   #     if(args.input != ""):  
+   #         command = args.input.split(',')
                             
-        if len(command) < 11:
-            print("error, to few arguments")
-            sys.exit()
-        if len(command) > 11:
-            print("error, to many arguments")
-            sys.exit()
+    #    if len(command) < 11:
+    #        print("error, to few arguments")
+    #        sys.exit()
+   #     if len(command) > 11:
+    #        print("error, to many arguments")
+    #        sys.exit()
 
-        test_amount = int(command[0])
+        test_amount = args.test_amount
 
         for i in range(test_amount):
             test_number += 1
             #random_input_generate_file(size_station, size_lines, size_trains, size_pa, sc_max, lc_max (lc=line capacity), ll_max (ll=line length),
             #                           tc_max (tc = train capacity), pgs_max (pgs = passenger group size), ptr_max(passenger target round))
             
-            generator.random_input_generate_file(int(command[1]), int(command[2]), int(command[3]), int(command[4]), int(command[5]), int(command[6]), int(command[7]), int(command[8]), int(command[9]), int(command[10])) 
+            generator.random_input_generate_file(args.size_stations, args.size_lines, args.size_trains, args.size_passengers, args.station_capacity_max, args.line_capacity_max, args.line_length_max, args.train_capacity_max, args.passenger_group_size_max, args.passenger_target_round)
+        
 
             p1 = subprocess.run('python -m abfahrt < output_generated.txt', capture_output=True,shell=True)
 
