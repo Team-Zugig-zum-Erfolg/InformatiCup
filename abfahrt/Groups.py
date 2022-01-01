f289349a974e2f4f32e34239d93ac5b81c0f967e
lukas <lukas-dreyer@outlook.de>
2022-01-02 00:27:38 +0100

    Added types in all function signatures in class Groups

--- abfahrt/Groups.py
+++ abfahrt/Groups.py
@@ -1,6 +1,6 @@
 # This module provides runtime support for type hints. The most fundamental support consists of the types Any, Union, Callable, TypeVar, and Generic. For a full specification, please see PEP 484. For a simplified introduction to type hints, see PEP 483. (https://docs.python.org/3/library/typing.html)
 from typing import List
-
+from typing import Tuple
 
 from abfahrt.classes.Station import Station
 from abfahrt.classes.Line import Line
@@ -9,12 +9,10 @@ from abfahrt.classes.Passenger import Passenger
 
 class Groups:
 
-    route = []
-
-    def __init__(self, passengers):
-
+    def __init__(self, passengers: List[Passenger]):
+        self.route = []
         if type(passengers) != list:
-            return False
+            return
         for passenger in passengers:
             route_number = 0
             added = 0
@@ -23,12 +21,11 @@ class Groups:
                     self.route[route_number].append(passenger)
                     added = 1
                     break
-                route_number = route_number + 1
+                route_number += 1
             if added == 0:
                 self.route.append([passenger])
 
-    def _get_min_target_round(self, group):
-
+    def _get_min_target_round(self, group: List[Passenger]) -> int:
         min = -1
         for pa in group:
             if min == -1:
@@ -38,40 +35,40 @@ class Groups:
                 min = pa.target_time
         return min
 
-    def get_priority(self):
-
+    def get_priority(self) -> List[Passenger]:
         if len(self.route) == 0:
             return None
-
         self.route.sort(key=self._get_min_target_round)
         return self.route[0]
 
-    def passengers_arrive(self, group):
-
+    def passengers_arrive(self, group: List[Passenger]) -> bool:
+        if group not in self.route:
+            return False
         self.route.remove(group)
-        return
+        return True
 
-    def get_passenger_with_most_size(self, group):
+    def get_passenger_with_most_time(self, group: List[Passenger]) -> Passenger:
         if len(group) == 0:
             return None
-        passenger_max_size = group[0]
+        passenger_max_time = group[0]
         for passenger in group:
-            if passenger.target_time > passenger_max_size.target_time:
-                passenger_max_size = passenger
-        return passenger_max_size
+            if passenger.target_time > passenger_max_time.target_time:
+                passenger_max_time = passenger
+        return passenger_max_time
 
-    def split_group(self, group):
-
+    def split_group(self, group: List[Passenger]) -> bool:
+        if group not in self.route:
+            return False
         self.route.remove(group)
 
-        passenger_with_max_size = self.get_passenger_with_most_size(group)
+        passenger_with_max_time = self.get_passenger_with_most_time(group)
 
-        group.remove(passenger_with_max_size)
+        group.remove(passenger_with_max_time)
 
         first_group = group
-        second_group = [passenger_with_max_size]
+        second_group = [passenger_with_max_time]
         if len(first_group) != 0:
             self.route.append(first_group)
         if len(second_group) != 0:
             self.route.append(second_group)
-        return
+        return True
