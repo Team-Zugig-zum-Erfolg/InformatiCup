8592f8f210eaa4b007850587eebd6d37bb152684
Berkehan Uenal <berkehan14@outlook.de>
2022-01-07 17:56:00 +0100

    added new comments to generator and update documentation.

--- abfahrt/Generator.py
+++ abfahrt/Generator.py
@@ -20,12 +20,12 @@ class Generator:
 
     def _list_to_class_object(self, list_properties: list, class_name: str, stations: list) -> any:
         """
-        Converts list object to specific class object
+        Converts list object to specific class object.
 
         Args:
-            list_properties (list): list of properties of the object
-            class_name (str): class name
-            stations (list): stations
+            list_properties (list): list of properties of the object.
+            class_name (str): class name.
+            stations (list): stations.
 
         Returns:
             any: class object
@@ -46,7 +46,7 @@ class Generator:
 
     def random_input_generate_as_classes(self, size_station=10, size_lines=20, size_trains=10, size_pa=10, sc_max=10, lc_max=2, ll_max=10, tc_max=20, pgs_max=10, ptr_max=10, max_speed_train=10) -> list:
         """
-        Generates random input and returns it as stations, lines, trains, passengers objects
+        Generates random input and returns it as stations, lines, trains, passengers objects.
 
         Args:
             size_station (int, optional): amount of stations. Defaults to 10.
@@ -62,7 +62,7 @@ class Generator:
             max_speed_train (int, optional): max train speed. Defaults to 10.
 
         Returns:
-            list: stations, lines, trains, passengers
+            list: stations, lines, trains, passengers.
         """
         stations = []
         lines = []
@@ -92,7 +92,7 @@ class Generator:
 
     def random_input_generate_file(self, size_station=10, size_lines=20, size_trains=10, size_pa=10, sc_max=10, lc_max=10, ll_max=10, tc_max=10, pgs_max=10, ptr_max=10, max_speed_train=10) -> None:
         """
-        Gnerates random input with entities stations, lines, trains, passengers and prints it in a file
+        Generates random input with entities stations, lines, trains, passengers and prints it in a file.
 
         Args:
             size_station (int, optional): amount of stations. Defaults to 10.
@@ -137,6 +137,15 @@ class Generator:
         out_file.close()
 
     def _write_objects_to_file(self, _objects: list, out_file) -> None:
+        """
+        writes objekts list into the out_file.
+
+        Args:
+            _objekt(list): List of all Generated _objekts.
+            
+        Returns:
+            None
+        """
         for _object in _objects:
             i = 1
             for attr in _object:
@@ -151,6 +160,17 @@ class Generator:
                 i += 1
 
     def _depth_search(self, station: int, station_lines: list, visited: list) -> None:
+        """
+        Depth search function to check if all stations are conected. 
+
+        Args:
+            station (int): id of Stations.
+            station_lines (list): list of all lines.
+            visited (list): list of visited stations.
+
+        Returns:
+            None
+        """
         visited[station] = True
         for next in station_lines[station]:
             if not visited[next]:
@@ -158,6 +178,16 @@ class Generator:
         return
 
     def _check_all_stations_connected(self, stations: list, station_lines: list) -> bool:
+        """
+        Check if all stations are connected. 
+
+        Args:
+            stations (list): list of all Stations.
+            station_lines (list): list of all lines.
+
+        Returns:
+            bool: are all stations connected?, true = They are connected, false = They aren't connected.
+        """
         visited = [True]
         for i in range(len(stations)):
             visited.append(False)
@@ -171,7 +201,7 @@ class Generator:
 
     def random_input_generate(self, size_station=10, size_lines=20, size_trains=10, size_pa=10, sc_max=10, lc_max=10, ll_max=10, tc_max=10, pgs_max=10, ptr_max=10, max_speed_train=10) -> list:
         """
-        Gnerates random input with entities stations, lines, trains, passengers as list objects
+        Generates random input with entities stations, lines, trains, passengers as list objects.
 
         Args:
             size_station (int, optional): amount of stations. Defaults to 10.
@@ -187,7 +217,7 @@ class Generator:
             max_speed_train (int, optional): max train speed. Defaults to 10.
 
         Returns:
-            list: stations, lines, trains, passengers
+            list: stations, lines, trains, passengers.
         """
         stations = []
         lines = []
@@ -277,14 +307,14 @@ class Generator:
 
     def random_station_generate(self, number, station_capacity_max):
         """
-        [summary]
+        Generates Random Station from args.
 
         Args:
-            number ([type]): [description]
-            station_capacity_max ([type]): [description]
+            number (int): station ID.
+            station_capacity_max (int): maximal Station capacity.
 
         Returns:
-            [type]: [description]
+            station: Returns a complete Station.
         """
         station = []
         station_name = "S" + str(number)
@@ -294,17 +324,17 @@ class Generator:
 
     def random_line_generate(self, number, stations, lines, line_capacity_max, line_length_max):
         """
-        [summary]
+        Generates Random Line from args.
 
         Args:
-            number ([type]): [description]
-            stations ([type]): [description]
-            lines ([type]): [description]
-            line_capacity_max ([type]): [description]
-            line_length_max ([type]): [description]
+            number (int): Line ID.
+            stations (list): list of stations.
+            lines (list): list of Lines.
+            line_capacity_max (int): maximal Line capaity.
+            line_length_max (int): maximal line length.
 
         Returns:
-            [type]: [description]
+            All Elements for a line: line_id, line_start, line_end, line_length and line_capacity.
         """
         size_stations = len(stations)
 
@@ -334,17 +364,17 @@ class Generator:
 
     def random_train_generate(self, number, stations, trains, train_capacity_max, max_speed_train):
         """
-        [summary]
+        Generates Random train from args.
 
         Args:
-            number ([type]): [description]
-            stations ([type]): [description]
-            trains ([type]): [description]
-            train_capacity_max ([type]): [description]
-            max_speed_train ([type]): [description]
+            number (int): Line ID.
+            stations (list): list of stations.
+            trains (list): list of trains.
+            train_capacity_max (int): maximal capacity of train.
+            max_speed_train (int): maximal speed of train.
 
         Returns:
-            [type]: [description]
+            All Elements for a train: train_id, train_start_station, train_speed, train_capacity.
         """
         size_stations = len(stations)
         train_id = "T" + str(number)
@@ -373,17 +403,17 @@ class Generator:
 
     def random_passenger_generate(self, number, stations, passengers, group_size, target_round):
         """
-        [summary]
+        Generates Random passenger from args.
 
         Args:
-            number ([type]): [description]
-            stations ([type]): [description]
-            passengers ([type]): [description]
-            group_size ([type]): [description]
-            target_round ([type]): [description]
+            number (int): passenger ID.
+            stations (list): list of stations.
+            passengers (list): list of passengers.
+            group_size (int): maximal passenger group size.
+            target_round (int): time passengers want to arrive. 
 
         Returns:
-            [type]: [description]
+            All Elements for a passenger: passenger_id, "S"+str(passenger_start_station), "S"+str(passenger_end_station), passenger_group_size, passenger_target_round.
         """
         size_stations = len(stations)
         passenger_id = "P" + str(number)
